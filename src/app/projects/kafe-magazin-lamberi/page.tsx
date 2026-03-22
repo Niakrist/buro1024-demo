27aae415fd3d1560a8a2a11cd5755851bd895690
Niakrist <ak_88dron@inbox.ru>
2026-03-22 10:51:41 +0300

    feat: emplement kafe-magazin-lamberi

---
+++ src/app/projects/kafe-magazin-lamberi/page.tsx
@@ -0,0 +1,36 @@
+import { Container, ImgContainer, NavMenu } from '@/ui';
+import styles from './page.module.css';
+import { Content, Header, Slider } from '@/components';
+import { projects } from '@/data';
+import {
+  adapterContent,
+  adapterGallery,
+  adapterSliderForProject,
+} from '@/utils';
+
+export default function KafeMagazinLamberiPage() {
+  const slides = adapterSliderForProject(projects[12]);
+  const content = adapterContent(projects[12]);
+  const gallery = adapterGallery(projects[12]);
+
+  return (
+    <div className={styles.page}>
+      <Header absolute color="white">
+        <NavMenu color="white" />
+      </Header>
+      <Slider slides={slides} />
+      <Content
+        title={content.title}
+        description={content.description}
+        tags={content.tags}
+      />
+      <Container className={styles.mb63}>
+        <ImgContainer
+          item1={gallery[0]}
+          item2={gallery[1]}
+          item3={gallery[2]}
+        />
+      </Container>
+    </div>
+  );
+}
