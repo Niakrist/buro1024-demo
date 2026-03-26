1b835a71c2f032a357f2c456421885118ec920ef
Niakrist <ak_88dron@inbox.ru>
2026-03-26 13:05:24 +0300

    feat: emplement about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,60 @@
+import { Button, Container, Htag, NavMenu, Text } from '@/ui';
+import styles from './page.module.css';
+import { Header } from '@/components';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Header color="black">
+        <NavMenu color="black" />
+      </Header>
+      <Container className={styles.container}>
+        <Htag color="black" size="big" tag="h1" hidden>
+          Контакты
+        </Htag>
+        <img className={styles.img} src="about.jpg" alt={'О нас'} />
+        <div className={styles.content}>
+          <Htag color="black" size="medium" tag="h2" className={styles.mb16}>
+            О нас
+          </Htag>
+          <Text color="black" size="medium" className={styles.mb8}>
+            Respace — архитектурное бюро, которое создает уникальные
+            пространства.
+          </Text>
+          <Text color="black" size="medium" className={styles.mb8}>
+            Мы работаем с архитектурой, интерьерами и средой, исходя не из
+            стиля, а из личности. Нам важно понять, для кого создается
+            пространство, как человек живет, чувствует и взаимодействует с ним
+            каждый день.
+          </Text>
+          <Text color="black" size="medium">
+            Мы не работаем по шаблонам и не продвигаем «единственно верный»
+            стиль. Каждый проект Respace — это индивидуальное решение,
+            сформированное задачей, контекстом и характером клиента.
+          </Text>
+        </div>
+
+        <div className={styles.contacts}>
+          <Htag color="black" size="medium" tag="h2" className={styles.mb16}>
+            Контакты
+          </Htag>
+          <Text color="black" size="medium">
+            Мы на связи с 10:00 до 20:00 по рабочим дням. Если у вас срочный
+            вопрос то лучше позвонить или написать в телеграм.
+          </Text>
+          <div className={styles.buttons}>
+            <Button bg="black" color="white" size="small">
+              Позвонить
+            </Button>
+            <Button bg="black" color="white" size="small">
+              Написать на почту
+            </Button>
+            <Button bg="black" color="white" size="small">
+              Написать в телеграм
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
--- src/components/Publication/Publication.tsx
+++ src/components/Publication/Publication.tsx
@@ -2,6 +2,7 @@ import { IPublication } from '@/types/publication.interface';
 import React from 'react';
 import styles from './Publication.module.css';
 import { Button, Htag, Text } from '@/ui';
+import cn from 'classnames';
 
 export interface IPublicationProps extends React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLDivElement>,
@@ -9,13 +10,24 @@ export interface IPublicationProps extends React.DetailedHTMLProps<
 > {
   publication: IPublication;
   type: 'small' | 'medium' | 'big';
+  className?: string;
+  buttonName: string;
 }
-export const Publication = ({ publication, type }: IPublicationProps) => {
+export const Publication = ({
+  publication,
+  type,
+  className,
+  buttonName,
+}: IPublicationProps) => {
   switch (type) {
     case 'big':
       return (
-        <div className={styles[type]}>
-          <img src={publication.img} alt={publication.title} />
+        <div className={cn(styles.wrapper, styles.mb120)}>
+          <img
+            className={cn(styles.img)}
+            src={publication.img}
+            alt={publication.title}
+          />
           <div className={styles.content}>
             <Htag color="white" tag="h2" size="medium" className={styles.mb16}>
               {publication.title}
@@ -23,12 +35,75 @@ export const Publication = ({ publication, type }: IPublicationProps) => {
             <Text size="medium" color="white" className={styles.mb32}>
               {publication.description}
             </Text>
-            <Button size="small" bg="black" color="white">
-              Читать полностью
+            <Button
+              size="small"
+              bg="black"
+              color="white"
+              href={`publications/${publication.url}`}
+            >
+              {buttonName}
             </Button>
           </div>
         </div>
       );
+    case 'medium':
+      return (
+        <div className={cn(styles.wrapper, className)}>
+          <img
+            className={cn(styles.img, styles.mb32)}
+            src={publication.img}
+            alt={publication.title}
+          />
+          <Htag color="black" tag="h2" size="medium" className={styles.mb16}>
+            {publication.title}
+          </Htag>
+          <Text size="medium" color="black" className={styles.mb32}>
+            {publication.description}
+          </Text>
+          <Button
+            size="small"
+            bg="black"
+            color="white"
+            href={`publications/${publication.url}`}
+          >
+            {buttonName}
+          </Button>
+        </div>
+      );
+    case 'small': {
+      return (
+        <div className={cn(styles.wrapper, className)}>
+          <img
+            className={cn(styles.imgSmall, styles.mb32)}
+            src={publication.img}
+            alt={publication.title}
+          />
+          <Htag
+            color="black"
+            tag="h2"
+            size="medium"
+            className={cn(styles.mb16, styles.titleSmall)}
+          >
+            {publication.title}
+          </Htag>
+          <Text
+            size="medium"
+            color="black"
+            className={cn(styles.mb32, styles.testSmall)}
+          >
+            {publication.description}
+          </Text>
+          <Button
+            size="small"
+            bg="black"
+            color="white"
+            href={`publications/${publication.url}`}
+          >
+            {buttonName}
+          </Button>
+        </div>
+      );
+    }
   }
 
   return <div>Publication</div>;
